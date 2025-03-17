0b1880b65401e217e1f164ae50666e74121ebc6b
Siddharth Tripathi <siddharthtripathi3824@gmail.com>
2025-03-17 10:20:12 +0530

    Update urls.py

--- debate_app/urls.py
+++ debate_app/urls.py
@@ -2,6 +2,17 @@ from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from debate import views
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.generic import TemplateView
+from django.contrib.sitemaps.views import sitemap
+from debate.sitemaps import StaticViewSitemap, DebateSitemap
+
+# Define sitemaps
+sitemaps = {
+    'static': StaticViewSitemap,
+    'debates': DebateSitemap,
+}
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -10,4 +21,11 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='debate/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='home'), name='logout'),
     path('signup/', views.signup, name='signup'),
+    # SEO URLs
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
+    path('robots.txt', TemplateView.as_view(template_name='robots.txt', content_type='text/plain')),
 ]
+
+# Serve static files during development
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
