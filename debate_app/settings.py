f21e9f140bfe6e5371754aebbf754bc4f2079e44
Siddharth Tripathi <siddharthtripathi3824@gmail.com>
2025-03-17 10:22:19 +0530

    Update settings.py

--- debate_app/settings.py
+++ debate_app/settings.py
@@ -117,7 +117,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+# Static files (CSS, JavaScript, Images)
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "debate/static",
+]
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
